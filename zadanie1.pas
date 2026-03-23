29fbdd1e77cb7ae7d2e799116eedf15a492990ca
Eryk Stępień <96150732+EriK013@users.noreply.github.com>
2026-03-23 12:09:00 +0100

    zadanie3_5
    Added BubbleSort procedure to sort an integer array.

--- zadanie1.pas
+++ zadanie1.pas
@@ -1,4 +1,10 @@
 program zadanie1;
+
+{$mode objfpc}{$H+}
+
+type
+    TIntArray = array of integer;
+
 procedure Random50;
 var
     i : integer;
@@ -16,6 +22,30 @@ begin
     writeln;
 end;
 
+procedure BubbleSort(var arr : TIntArray);
+
+var
+    i, j, k, temp : integer;
 begin
-    Random50;
+    i := High(arr);
+    if i < 0 then Exit;
+
+    repeat
+        j := 0;
+        for k := 1 to i do
+          begin
+            if arr[k - 1] > arr[k] then
+              begin 
+                temp := arr[k - 1];
+                arr[k - 1] := arr[k];
+                arr[k] := temp;
+                j := k;
+              end;
+          end;
+        i := j;
+    until i <= 0;
+end;
+
+begin
+    Randomize;
 end.
