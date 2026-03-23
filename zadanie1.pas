255f616e6d556f843b51fd096e59058356743676
Eryk Stępień <96150732+EriK013@users.noreply.github.com>
2026-03-23 12:12:45 +0100

    zadanie4_5
    Added a Test procedure to validate RandomNumbers and BubbleSort functionality.

--- zadanie1.pas
+++ zadanie1.pas
@@ -1,4 +1,5 @@
 program zadanie1;
+
 {$mode objfpc}{$H+}
 
 type
@@ -44,6 +45,88 @@ begin
     until i <= 0;
 end;
 
+procedure Test;
+var
+    arrTest : TIntArray;
+    i, passed : integer;
+    testFlag : Boolean;
+begin
+    passed := 0;
+
+    writeln('TEST 1: poprawna liczba elementów');
+    RandomNumbers(0, 100, 10, arrTest);
+    if Length(arrTest) = 10 then 
+    begin
+        writeln('TEST 1: OK');
+        passed := passed + 1;
+    end
+    else
+        writeln('TEST 1: Błędna długość tablicy');
+    
+    testFlag := True;
+    writeln('TEST 2: poprawny zakres losowanych liczb');
+    RandomNumbers(10, 20, 50, arrTest);
+    for i := 0 to High(arrTest) do
+    begin
+        if (arrTest[i] < 10) or (arrTest[i] > 20) then
+        begin
+            writeln('TEST 2: Błąd zakresu liczb: ', arrTest[i]);
+            testFlag := False;
+            Break;
+        end;
+    end;
+    if testFlag then
+    begin
+        writeln('TEST 2: OK');
+        passed := passed + 1;
+    end;
+
+    testFlag := True;
+    writeln('TEST 3: poprawne sortowanie');
+    RandomNumbers(0, 100, 20, arrTest);
+    BubbleSort(arrTest);
+    for i := 0 to High(arrTest) - 1 do
+    begin
+        if arrTest[i] > arrTest[i + 1] then
+        begin
+            writeln('TEST 3: Błąd sortowania: ', arrTest[i], ' > ', arrTest[i + 1]);
+            testFlag := False;
+            Break;
+        end;
+    end;
+    if testFlag then
+    begin
+        writeln('TEST 3: OK');
+        passed := passed + 1;
+    end;
+
+    writeln('TEST 4: sortowanie pustej tablicy');
+    RandomNumbers(0, 100, 0, arrTest);
+    BubbleSort(arrTest);
+    if Length(arrTest) = 0 then
+    begin
+        writeln('TEST 4: OK');
+        passed := passed + 1;
+    end
+    else
+        writeln('TEST 4: Błąd sortowania pustej tablicy');
+
+    writeln('TEST 5: sortowanie tablicy z jednym elementem');
+    RandomNumbers(0, 100, 1, arrTest);
+    BubbleSort(arrTest);
+    if Length(arrTest) = 1 then
+    begin
+        writeln('TEST 5: OK');
+        passed := passed + 1;
+    end
+    else
+        writeln('TEST 5: Błąd sortowania tablicy z jednym elementem');
+
+
+    writeln('Zakończono testy. Poprawnych: ', passed, '/5');
+end;
+
 begin
     Randomize;
+    Test;
 end.
